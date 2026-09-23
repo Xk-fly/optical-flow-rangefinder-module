405fec0e0225fb8249a980da783a6ad6f829db32
Xk-fly <2918599980@qq.com>
2026-09-23 12:51:43 +0800

    feat: classify VL53 read outcomes

---
+++ Core/Inc/VL53L1X/vl53_types.h
@@ -0,0 +1,17 @@
+#ifndef VL53_TYPES_H
+#define VL53_TYPES_H
+
+#include <stdint.h>
+
+#define VL53_REAL_MIN_DISTANCE_MM 50U
+#define VL53_REAL_MAX_DISTANCE_MM 3600U
+
+typedef enum {
+    VL53_READ_NOT_READY = 0,
+    VL53_READ_VALID = 1,
+    VL53_READ_TOO_CLOSE = 2,
+    VL53_READ_TOO_FAR = 3,
+    VL53_READ_ERROR = 4
+} VL53ReadResult;
+
+#endif
