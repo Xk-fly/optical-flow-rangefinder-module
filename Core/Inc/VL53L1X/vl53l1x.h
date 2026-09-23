2022eab269d8155e9a970758b278b5b9107fa6da
Xk-fly <2918599980@qq.com>
2026-09-23 12:51:45 +0800

    refactor: expose typed VL53 read status

--- Core/Inc/VL53L1X/vl53l1x.h
+++ Core/Inc/VL53L1X/vl53l1x.h
@@ -2,11 +2,12 @@
 #define VL53L1X_H
 
 #include "VL53L1X/core/VL53L1X_api.h"
+#include "VL53L1X/vl53_types.h"
 #include "main.h"
 
 #define VL53_DISTANCE_FRESH_TIMEOUT_MS 300U
 
 uint8_t vl53_Init(void);
-uint8_t vl53_GetDistance(uint16_t *distance_mm);
+VL53ReadResult vl53_GetDistance(uint16_t *distance_mm);
 
 #endif
